c48bd71e97ed4b266d7fa4df89c737c4b27f6e72
EduardoHenriques <fernando28henriques@gmail.com>
2023-10-25 21:49:47 +0100

    assignment_server_client

---
+++ code/client.py
@@ -0,0 +1,17 @@
+import socket
+import time
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_ip = "10.0.4.10"
+server_port = 1223 # UDP
+
+client_socket.connect((server_ip, server_port))
+i = 1
+while i <= 5:
+    message = f"Datagrama Teste {i}"
+    client_socket.send(message.encode('utf-8'))
+    print("PACKET ENVIADO")
+    i +=1
+    time.sleep(3)
+client_socket.close()
---
+++ code/server.py
@@ -0,0 +1,28 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "10.0.4.10"
+UDP_port = 1223
+server_socket.bind((host, UDP_port))
+
+server_socket.listen(5)
+
+print(f"A ouvir em {host}:{UDP_port}")
+
+client_socket, client_address = server_socket.accept()
+print(f"Ligação de {client_address}")
+
+packets = 0
+
+while True:
+    data = client_socket.recv(1024)
+    if not data:
+        break
+    print(f"Recebeu: {data.decode('utf-8')}")
+    packets +=1
+    if packets == 5:
+        break
+
+client_socket.close()
+server_socket.close()
