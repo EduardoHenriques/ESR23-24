a54c4ca07b9724a5d226557993f7199f489a19d3
EduardoHenriques <fernando28henriques@gmail.com>
2023-10-25 18:44:07 +0100

    fix

--- code/servers.py
+++ code/servers.py
@@ -4,4 +4,4 @@ if __name__=="__main__":
     with open("servers.json","r") as file:
         data = json.load(file)
         for router in data["routers"]:
-            print(router["neighbours"])
+            print(router["name"] + " neighbours => " + ", ".join(router["neighbours"]))
