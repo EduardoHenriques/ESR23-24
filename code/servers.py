50a1aad8aea3d60607931d4185eb5f24165aac9f
EduardoHenriques <fernando28henriques@gmail.com>
2023-10-25 18:23:35 +0100

    initial commit

---
+++ code/servers.py
@@ -0,0 +1,7 @@
+import json
+
+if __name__=="__main__":
+    with open("servers.json","r") as file:
+        data = json.load(file)
+        for router in data["routers"]:
+            print(router["neighbours"])
