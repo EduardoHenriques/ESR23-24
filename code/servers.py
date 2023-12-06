ee08f3def4da09195cc5627b7203f08c249c373d
Pedro Freitas <144889630+Pedro7Freitas7@users.noreply.github.com>
2023-12-06 20:04:27 +0000

    Delete code/servers.py
    Servers.py Deleted

--- code/servers.py
+++
@@ -1,9 +0,0 @@
-import json
-
-if __name__=="__main__":
-    with open("servers.json","r") as file:
-        data = json.load(file)
-        for router in data["routers"]:
-            print(router["name"] + " neighbours => " + ", ".join(router["neighbours"]))
-            
-
